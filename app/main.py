c3623e7478f9e96e9ed1002581c72cf899417ad3
SantiagoRS26 <santiagors2611@gmail.com>
2024-11-05 23:32:41 -0500

    config cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Traffic Sign Recognition API")
 app.include_router(router)
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar el dominio del frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Puntos de entrada para ejecutar Uvicorn si se ejecuta este archivo directamente
 if __name__ == "__main__":
